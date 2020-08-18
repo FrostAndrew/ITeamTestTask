c89e16f41c23bfdb2bdc4bf3af6cfd31f9dc4381
Andrew Vorobiov <gorobchik2002@gmail.com>
2020-08-18 20:25:21 +0300

    Main mechanism is done.

---
+++ src/app/stopwatch/stopwatch.component.ts
@@ -0,0 +1,108 @@
+import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {fromEvent, Observable, Subscriber, Subscription} from "rxjs";
+import {buffer, debounce, debounceTime, filter, map, share} from "rxjs/operators";
+
+interface StopwatchState {
+  seconds: number;
+  minutes: number;
+  hours: number;
+
+  isTicks: boolean;
+  textFormat: string;
+}
+
+@Component({
+  selector: 'app-stopwatch',
+  templateUrl: './stopwatch.component.html',
+  styleUrls: ['./stopwatch.component.scss']
+})
+export class StopwatchComponent implements AfterViewInit{
+  @ViewChild('DoubleClickWaitButton', {static: false}) waitButton : ElementRef;
+
+  state: StopwatchState;
+  timeStream$: Observable<void>;
+  clickStream$: Observable<any>;
+  doubleClickSub: Subscription;
+  timeSub: Subscription;
+  observer: any;
+
+  constructor() {
+    this.timeStream$ = new Observable<void>((observer) => {
+      const interval = setInterval(
+        _ => observer.next(),
+        1000
+      );
+      return () => {clearInterval(interval); };
+    });
+
+    this.observer = {
+      next: (_) => this.StateTick(),
+      error: err => console.error('An observer error:' + err),
+      complete: () => alert('Complete')
+    };
+    this.state = {
+      seconds: 0,
+      minutes: 0,
+      hours: 0,
+      isTicks: false,
+      textFormat: '00:00:00'
+    };
+
+  }
+
+  ngAfterViewInit(): void {
+    this.clickStream$ = fromEvent(this.waitButton.nativeElement, 'click');
+    this.clickStream$.subscribe();
+
+    this.doubleClickSub = this.clickStream$.pipe(
+      buffer( this.clickStream$.pipe(debounceTime(299))),
+      map(clickArr => clickArr.length),
+      filter(numberOfClicks => numberOfClicks == 2)
+    ).subscribe(_ => this.state.isTicks? this.StartStopTime(false) : null);
+  }
+
+  async StateTick(): Promise<void> {
+    if(this.state.seconds + 1 == 60) {
+      if(this.state.minutes + 1 == 60) {
+        this.state.hours++;
+        this.state.minutes = 0;
+      } else this.state.minutes++;
+      this.state.seconds = 0;
+      this.state.textFormat = this.GetBeautifiedTime();
+      return;
+    }
+    this.state.seconds++;
+    this.state.textFormat = this.GetBeautifiedTime();
+    console.log(this.state);
+  }
+  async NullStopWatch(): Promise<void> {
+    this.state.seconds = this.state.minutes = this.state.hours = 0;
+    this.state.textFormat = this.GetBeautifiedTime();
+  }
+
+  StartStopTime(isZeroingWatch: boolean): void {
+    this.state.isTicks = !this.state.isTicks;
+    if(this.timeSub == null || this.timeSub.closed) {
+      this.timeSub = this.timeStream$.subscribe(this.observer);
+      console.log('Subscribed');
+      return;
+    }
+    this.timeSub.unsubscribe();
+
+    isZeroingWatch? this.NullStopWatch() : console.log('Paused');
+    console.log('Unsubscribed');
+  }
+
+  GetBeautifiedTime(): string {
+    let str: string = '';
+
+    if (this.state.hours < 10) str += '0';
+    str += this.state.hours + ':';
+    if (this.state.minutes < 10) str += '0';
+    str += this.state.minutes + ':';
+    if (this.state.seconds < 10) str += '0';
+    str += this.state.seconds;
+
+    return str;
+  }
+}
